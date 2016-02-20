3d677893d1de53969ce6569e97d90e31124b0150
Accommodavid <davidvgdn@me.com>
2016-02-20 23:59:12 +0100

    Add no.dots style and README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bubbles.js
+
+Used on the [no.dots website][1]. Makes SVG bubbles inside a figure. See the [demo][2].
+
+[1]: http://nodots.nl/
+[2]: https://no-dots.github.io/bubbles.js/
--- bubbles.js
+++ bubbles.js
@@ -5,9 +5,10 @@
  * Makes bubbles.
  * Requires an element on the page with id #bubbles and a child SVG element
  */
-document.addEventListener('DOMContentLoaded', function ($e) {
+document.addEventListener('DOMContentLoaded', function () {
   // The time each circle stays on the screen
-  var delayTime = 500
+  bubbles.delayTime = 1000
+  bubbles.maxRadius = 75
 
   bubbles.addEventListener('mousemove', function (event) {
     // Create circle
@@ -16,12 +17,17 @@ document.addEventListener('DOMContentLoaded', function ($e) {
     // Set attributes
     $el.setAttribute('cx', event.pageX - bubbles.offsetLeft)
     $el.setAttribute('cy', event.pageY - bubbles.offsetTop)
-    $el.setAttribute('r', Math.random() * 50)
+    $el.setAttribute('r', 0)
 
     // Hide circle after specified time
     window.setTimeout(function () {
       $el.setAttribute('r', 0)
-    }, delayTime)
+    }, bubbles.delayTime)
+
+    // Make circle grow afterwards
+    window.setTimeout(function () {
+      $el.setAttribute('r', Math.random() * bubbles.maxRadius)
+    }, bubbles.delayTime / 10)
 
     // Append element
     document.getElementsByTagName('svg')[0].appendChild($el)
